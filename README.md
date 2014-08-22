92d394ce5860fb351b6d4ed06ecce3f96ef26886
Can Bülbül <alicanblbl@gmail.com>
2014-08-22 20:58:26 +0300

    added files

---
+++ README.md
@@ -0,0 +1,6 @@
+Sudoku Solver
+================
+
+A byproduct of my experiments on constraint programming.
+I provided some samples for testing. To use, type
+``python sudoku.py sample<number>.txt ``. It will print the formatted output of the solution.
---
+++ sudoku.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os,sys,constraint
+
+# a text file that contains 9x9 digits seperated with whitespace.
+# empty squares are denoted by zero.
+data = map(int,open(sys.argv[1]).read().split())
+#3x3 square template
+square = [0,1,2,9,10,11,18,19,20]
+
+sudoku = constraint.Problem()
+#[sudoku.addConstraint(lambda x: x==e,[i]) for i,e in enumerate(data) if e]
+sudoku.addVariables(range(81),range(1,10))
+for i,e in enumerate(data):
+  if e: del sudoku._variables[i] 
+#initial values
+[sudoku.addVariable(i,[e]) for i,e in enumerate(data) if e]
+
+#row constraint
+[sudoku.addConstraint(constraint.AllDifferentConstraint(),[9*row+p for p in range(9)]) for row in range(9)]
+#column constraint
+[sudoku.addConstraint(constraint.AllDifferentConstraint(),[row+9*p for p in range(9)]) for row in range(9)]
+#3x3 square constraint
+[sudoku.addConstraint(constraint.AllDifferentConstraint(),map(lambda x:x+o,square)) for o in map(lambda x:x*3,square)]
+
+#pure witchcraft
+solution = sudoku.getSolution()
+
+#output formatting
+out = ""
+vals = solution.values()
+for e in xrange(81):
+  if (e+1)%9:
+    out += "%d "%vals[e]
+  else: out += "%d\n"%vals[e]
+
+print out

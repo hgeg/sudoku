b35e56191b864fc1368e65c10cbf70f1fa2505ff
Can Bülbül <alicanblbl@gmail.com>
2014-08-25 12:29:02 +0300

    made main file executable

--- sudoku.py
+++ sudoku.py
@@ -1,35 +1,44 @@
 #!/usr/bin/env python
 import os,sys,constraint
 
-# a text file that contains 9x9 digits seperated with whitespace.
-# empty squares are denoted by zeroes.
-data = map(int,open(sys.argv[1]).read().split())
-#3x3 square template
-square = [0,1,2,9,10,11,18,19,20]
+def main():
+  # a text file that contains 9x9 digits seperated with whitespace.
+  # empty squares are denoted by zeroes.
+  data = map(int,open(sys.argv[1]).read().split())
+  #3x3 square template
+  square = [0,1,2,9,10,11,18,19,20]
 
-sudoku = constraint.Problem()
+  sudoku = constraint.Problem()
 
-#add free variables
-sudoku.addVariables(filter(lambda x: data[x]==0, range(81)),range(1,10))
-#limit the domain for given values
-[sudoku.addVariable(i,[e]) for i,e in enumerate(data) if e]
+  #add free variables
+  sudoku.addVariables(filter(lambda x: data[x]==0, range(81)),range(1,10))
+  #limit the domain for given values
+  [sudoku.addVariable(i,[e]) for i,e in enumerate(data) if e]
 
-#row constraint
-[sudoku.addConstraint(constraint.AllDifferentConstraint(),[9*row+p for p in range(9)]) for row in range(9)]
-#column constraint
-[sudoku.addConstraint(constraint.AllDifferentConstraint(),[row+9*p for p in range(9)]) for row in range(9)]
-#3x3 square constraint
-[sudoku.addConstraint(constraint.AllDifferentConstraint(),map(lambda x:x+o,square)) for o in map(lambda x:x*3,square)]
+  #row constraint
+  [sudoku.addConstraint(constraint.AllDifferentConstraint(),[9*row+p for p in range(9)]) for row in range(9)]
+  #column constraint
+  [sudoku.addConstraint(constraint.AllDifferentConstraint(),[row+9*p for p in range(9)]) for row in range(9)]
+  #3x3 square constraint
+  [sudoku.addConstraint(constraint.AllDifferentConstraint(),map(lambda x:x+o,square)) for o in map(lambda x:x*3,square)]
 
-#pure witchcraft
-solution = sudoku.getSolution()
+  #pure witchcraft
+  solution = sudoku.getSolution()
 
-#output formatting
-out = ""
-vals = solution.values()
-for e in xrange(81):
-  if (e+1)%9:
-    out += "%d "%vals[e]
-  else: out += "%d\n"%vals[e]
+  #check if solution exists
+  if not solution: 
+    print "No solution found!"
+    return 0
 
-print out
+  #output formatting
+  out = ""
+  vals = solution.values()
+  for e in xrange(81):
+    if (e+1)%9:
+      out += "%d "%vals[e]
+    else: out += "%d\n"%vals[e]
+
+  print out
+  return 0
+
+if __name__ == '__main__': main()
